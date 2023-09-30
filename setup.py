3df42bfae45d0a2368be9ee14bcfa1dcc51b37c5
RowDog <mrdiamonddirt@gmail.com>
2023-09-30 02:56:38 +0100

    url format

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.2',
     description='A flask server for a chrome extension that querys llm models',
     long_description='A flask server for a chrome extension that querys llm models for use with this chrome extention: https://chrome.google.com/webstore/detail/local-llama-llm-ai-chat-q/ekobbgdgkocdnnjoahoojakmoimfjlbm',
-    url='https: // github.com/mrdiamonddirt/local-llama-chrome-extension',
+    url='https://github.com/mrdiamonddirt/local-llama-chrome-extension',
     author='Rowan Wood',
     author_email='mrdiamonddirt@gmail.com',
     packages=find_packages(),
