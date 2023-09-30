fe73bdc7bc5cc22af22a9b2cd81c6272f8cdd927
RowDog <mrdiamonddirt@gmail.com>
2023-09-30 02:52:10 +0100

    added long description

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='local-llama',  # Change this to a suitable name for your script
-    version='1.0.1',
+    version='1.0.2',
     description='A flask server for a chrome extension that querys llm models',
+    long_description='A flask server for a chrome extension that querys llm models for use with this chrome extention: https://chrome.google.com/webstore/detail/local-llama-llm-ai-chat-q/ekobbgdgkocdnnjoahoojakmoimfjlbm',
+    url='https: // github.com/mrdiamonddirt/local-llama-chrome-extension',
     author='Rowan Wood',
     author_email='mrdiamonddirt@gmail.com',
     packages=find_packages(),
